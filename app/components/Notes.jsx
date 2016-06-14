f8dfbef17bfec4ae853d2010fad955059fba9668
Tendai Mutunhire <tmutunhire@gmail.com>
2016-06-14 18:29:19 +0200

    use uuid

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
+import uuid from 'uuid';
 
 const notes = [
   {
-    id: '4e81fc6e-bfb6-419b-93e5-0242fb6f3f6a',
+    id: uuid.v4(),
     task: 'Learn React'
   },
   {
-    id: '11bbffc8-5891-4b45-b9ea-5c99aadf870f',
+    id: uuid.v4(),
     task: 'Do laundry, for free!'
   },
   {
-    id: '12bbffc8-5891-4b45-b9ea-5c99aadf870f',
+    id: uuid.v4(),
     task: 'Master the guitar!'
   },
   {
-    id: '13bbffc8-5891-4b45-b9ea-5c99aadf870f',
+    id: uuid.v4(),
     task: 'Build apps!'
   },
   {
-    id: '14bbffc8-5891-4b45-b9ea-5c99aadf870f',
+    id: uuid.v4(),
     task: 'Go on tour!'
-  }
+  },
 ];
 
 export default () => (
