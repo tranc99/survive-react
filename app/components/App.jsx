74c833147f7c46d0fa44ad96f5a91348c73ccf9d
Tendai Mutunhire <tmutunhire@gmail.com>
2016-06-15 02:36:35 +0200

    deleting notes like a machine

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,32 +2,58 @@ import React from 'react';
 import uuid from 'uuid';
 import Notes from './Notes';
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry, for free!'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Master the guitar!'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Build apps!'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Go on tour!'
-  }
-];
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default () => (
-  <div>
-    <button onClick={() => console.log('add note')}>+</button>
-    <Notes notes={notes} />
-  </div>
-);
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry, for free!'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Master the guitar!'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Build apps!'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Go on tour!'
+        }
+      ]
+    };
+  }
+
+  addNote = () => {
+    console.log('add note');
+    this.setState({notes: this.state.notes.concat([{id: uuid.v4(), task: 'New Task'}])});
+  }
+
+  deleteNote = (id, e) => {
+    e.stopPropagation();
+
+    this.setState({
+      notes: this.state.notes.filter(note => note.id !== id)
+    });
+  }
+
+  render() {
+    const {notes} = this.state;
+
+    return(
+      <div>
+        <button onClick={this.addNote}>+</button>
+        <Notes notes={notes} onDelete={this.deleteNote} />
+      </div>
+    );
+  }
+
+}
--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Note from './Note';
 
-export default ({notes}) => (
-  <ul>{notes.map(note =>
-    <li key={note.id}>{note.task}</li>
+export default ({notes, onDelete=() => {}}) => (
+  <ul>{notes.map(({id, task}) =>
+    <li key={id}>
+      <Note task={task} onDelete={onDelete.bind(null, id)}/>
+    </li>
   )}
   </ul>
 )
